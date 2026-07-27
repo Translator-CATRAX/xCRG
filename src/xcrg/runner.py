c3e7c429c4da2b21b3d2f869131ee0d853a8168e
Jonathan Pettinger <jonathanpettinger@protonmail.com>
2026-07-27 15:04:59 -0700

    Document blocked gene curie

--- src/xcrg/runner.py
+++ src/xcrg/runner.py
@@ -58,6 +58,10 @@ from .utilities import (
 
 
 TF_QNODE_ID = "tf"
+# TP53 is a "master regulator" and regulates hundreds of genes.
+# We were finding it showed up everywhere (in paths of all lengths).
+# It's manually removed so we don't drown in X<-TP53->Y paths.
+# Maybe with better ranking, we could leave it in there, but for now, it's manually removed
 TP53_CURIE = "NCBIGene:7157"
 DIRECT_QEDGE_ID = "direct"
 
